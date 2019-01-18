ca819c3622a5bc26d67a4e2535c1c83edf66ab69
Grace Yang <ygsunshine86@gmail.com>
2019-01-17 20:43:20 -0800

    Add files via upload

--- Project/serve/predict.py
+++ Project/serve/predict.py
@@ -84,11 +84,12 @@ def predict_fn(input_data, model):
     data = data.to(device)
 
     # Make sure to put the model into evaluation mode
+    
     model.eval()
 
     # TODO: Compute the result of applying the model to the input data. The variable `result` should
     #       be a numpy array which contains a single integer which is either 1 or 0
-
-    result = np.round(output.numpy())
+    output=model(data)
+    result = (output.data).cpu().numpy().round()
 
     return result
